9213c58c542272639dad4b48133d3099ebf90346
sample tarou <sample@example.com>
2026-03-15 12:14:53 +0900

    USER_LISTのtype型を作成

---
+++ src/types/user.tsx
@@ -0,0 +1,31 @@
+export type Role = "student" | "mentor"
+
+export type BaseUser = {
+  id: number
+  name: string
+  role: Role
+  email: string
+  age: number
+  postCode: string
+  phone: string
+  hobbies: string[]
+  url: string
+}
+
+export type Student = BaseUser & {
+  role: "student"
+  studyMinutes: number
+  taskCode: number
+  studyLangs: string[]
+  score: number
+}
+
+export type Mentor = BaseUser & {
+  role: "mentor"
+  experienceDays: number
+  useLangs: string[]
+  availableStartCode: number
+  availableEndCode: number
+}
+
+export type User = Student | Mentor
