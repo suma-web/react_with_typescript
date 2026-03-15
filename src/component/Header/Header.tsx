29f9c15aa6a8958a2e45c31ce15887118a979543
sample tarou <sample@example.com>
2026-03-15 15:44:11 +0900

    Headerコンポーネントのnav修正

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -2,28 +2,10 @@ export const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTogglerDemo01"
-          aria-controls="navbarTogglerDemo01"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a className="navbar-brand" href="#">
+        <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarTogglerDemo01">
+          <h1 className="navbar-brand">
             User-Lists
-          </a>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                新しく追加
-              </a>
-            </li>
-          </ul>
+          </h1>
           <form className="d-flex" role="search">
             <input
               className="form-control me-2"
