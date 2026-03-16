d261d43d8f3a07b85c931727e9e4f987c4783b6c
sample tarou <sample@example.com>
2026-03-16 21:34:42 +0900

    変数名の変更

---
+++ src/component/ListEdit/ListEdit.tsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { userLists } from '../../data/userLists';
+import { ListAdd } from '../List/ListAdd';
+import type { User } from '../../types/user';
+import { useState } from 'react';
+import { ListShow } from '../List/ListShow';
+
+export const ListEdit = () => {
+  const [userList, setUserList] = useState<User[]>(userLists);
+
+  return (
+    <div className='container'>
+      <ListAdd setUserList={setUserList} />
+      <ListShow userList={userList}/>
+    </div>
+  );
+};
