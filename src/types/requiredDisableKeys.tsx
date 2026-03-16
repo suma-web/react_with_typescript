da649016891f30ae51c79dace004f08b97026981
sample tarou <sample@example.com>
2026-03-16 21:35:23 +0900

    student、mentorのkeyを分けて管理

---
+++ src/types/requiredDisableKeys.tsx
@@ -0,0 +1,9 @@
+export const studentRequired = [
+  "id","name","email","age","postCode","phone","hobbies","url",
+  "studyMinutes","taskCode","studyLangs","score"
+]
+
+export const mentorRequired = [
+  "id","name","email","age","postCode","phone","hobbies","url",
+  "experienceDays","useLangs","availableStartCode","availableEndCode"
+]
