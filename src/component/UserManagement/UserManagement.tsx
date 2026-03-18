c1c085d7ead3d75207b7d9f5e00abba9c72c3bbc
sample tarou <sample@example.com>
2026-03-18 22:00:13 +0900

    コンポーネント名の修正

--- src/component/UserManagement/UserManagement.tsx
+++ src/component/UserManagement/UserManagement.tsx
@@ -1,17 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { userLists } from '../../data/userLists';
-import { ListAdd } from '../List/UserForm';
+import { userList } from '../../data/userList';
+import { UserForm } from '../List/UserForm';
 import type { User } from '../../types/user';
 import { useState } from 'react';
-import { ListShow } from '../List/UserList';
+import { UserList } from '../List/UserList';
 
-export const ListEdit = () => {
-  const [userList, setUserList] = useState<User[]>(userLists);
+export const UserManagement = () => {
+  const [userListItem, setUserListItem] = useState<User[]>(userList);
 
   return (
-    <div className='container'>
-      <ListAdd setUserList={setUserList} />
-      <ListShow userList={userList}/>
+    <div className="container">
+      <UserForm setUserListItem={setUserListItem} />
+      <UserList userList={userListItem} />
     </div>
   );
 };
