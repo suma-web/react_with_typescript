43d66611386f17673771e3ad068c8492f2a168d4
sample tarou <sample@example.com>
2026-03-19 17:11:26 +0900

    不要なコードの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Header } from './components/Header/Header'
-import type { User } from './types/user'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { UserManagement } from './components/UserManagement/UserManagement'
