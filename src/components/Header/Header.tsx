126dd780c066bddfa28ab910e2f88f18d2c3b8ad
sample tarou <sample@example.com>
2026-03-19 17:10:28 +0900

    ブランチを新しく作成し実装のやり直し

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse d-flex justify-content-between" id="navbarTogglerDemo01">
+          <h1 className="navbar-brand">
+            User-Lists
+          </h1>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
